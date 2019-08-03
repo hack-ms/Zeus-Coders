b6b1bc701793ad1f471ad96e8077e662e32cd02e
zeuscoders <zeuscoders@gmail.com>
2019-08-03 17:26:56 -0400

    Gerar grafico com dados do banco

--- getScriptToList.py
+++ getScriptToList.py
@@ -7,6 +7,8 @@ def GetScriptFromDBToList():
     path = os.path.join(projectPath, "sqlserver.config")
     sqlConfig = open(path).read()
     
-    path = projectPath + "\\SqlScripts\\ScriptEscolasComMaioresMenoresNotasPorMunicipio.sql"
+    path = projectPath + "\\SqlScripts\\ScriptIndiceReprovacao.sql"
     listProp = GetRowsFromDB(sqlConfig, path)
     sys.stdout.write("\nTotal de coordenadas = " + str(len(listProp)) + "\n\n")
+    return listProp
+    
